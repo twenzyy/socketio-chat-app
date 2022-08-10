46ce7c6108a8216f7ca2cc5298a155c6987917ae
twn <40640590+twenzyy@users.noreply.github.com>
2022-08-10 13:16:07 +0300

    Change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const socket = require('socket.io')
 
 const app = express()
-const server = app.listen(3000)
+const server = app.listen(90)
 
 app.use(express.static('src'))
 
