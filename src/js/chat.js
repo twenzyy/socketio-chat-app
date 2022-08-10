58ee46f65a82fd3c0e1fc614aff55ed3af47f690
twn <40640590+twenzyy@users.noreply.github.com>
2022-08-10 13:03:41 +0300

    Socket.io Chat App

---
+++ src/js/chat.js
@@ -0,0 +1,36 @@
+const socket = io.connect('url')
+
+const sender = document.getElementById('sender')
+const message = document.getElementById('message')
+const output = document.getElementById('output')
+const feedback = document.getElementById('feedback')
+var form = document.getElementById('form');
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (message.value && sender.value) {
+        socket.emit('chat', {
+            message: message.value,
+            sender: sender.value
+        })
+      message.value = '';
+    }
+  });
+
+message.addEventListener('keypress', () =>{
+    socket.emit('typing', sender.value)
+})
+
+  socket.on('chat', function(data) {
+    output.innerHTML += `<p><strong> ${data.sender} : </strong> ${data.message} </p>`
+    window.scrollTo(0, document.body.scrollHeight);
+    feedback.innerHTML = "";
+  });
+
+socket.on('typing', data => {
+    feedback.innerHTML = `<p> ${data} yazıyor...`
+    setTimeout(() => {
+        feedback.innerHTML = "";
+    }, 6000);
+})
